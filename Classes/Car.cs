9e542d6758af42481cacfbf9bead316b45b0e2fd
Shaun Rudisail <sprudisail@gmail.com>
2025-06-24 21:22:18 -0400

    Completed the first exercise for Classes.

---
+++ Classes/Car.cs
@@ -0,0 +1,8 @@
+namespace Classes;
+
+public class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var newCar = new Car()
+            {
+                Make = "Honda",
+                Model = "Civic",
+                Year = 1993
+            };
+
+            Console.WriteLine("This was my first car's year, make, and model:");
+            Console.WriteLine(newCar.Year);
+            Console.WriteLine(newCar.Make);
+            Console.WriteLine(newCar.Model);
         }
     }
 }
